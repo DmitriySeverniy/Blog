cffe6e27f10c89eaa46f028d535b01d6cc008982
DmitriySeverniy <tryasunoff.d@yandex.ru>
2022-08-26 14:38:17 +0300

    fonts

--- templates/admin/include/header.php
+++ templates/admin/include/header.php
@@ -1,5 +1,5 @@
       <div id="adminHeader">
-        <h2>Какой-то Кот администратор</h2>
+        <h2>Какой-то администратор</h2>
         <p>Вы вошли в систему как <b><?php echo htmlspecialchars( $_SESSION['username']) ?></b>. <a href="admin.php?action=listArticles">Редактировать статьи</a> <a href="admin.php?action=listCategories">Редактировать категории</a> <a href="admin.php?action=logout"?>Выйти из системы</a></p>
       </div>
 
--- templates/include/footer.php
+++ templates/include/footer.php
@@ -2,6 +2,7 @@
         Какой-то Кот &copy; 2022. Все права защищены. <a href="admin.php">Администратор Кот</a>
       </div>
     </div>
+    <script src="js/script.js"></script>
   </body>
 </html>
 
